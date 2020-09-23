bab5fb7bec2935e8890e909568cf9fd952d85757
siddu <siddu@siddhjtech.com>
2020-09-23 20:57:03 +0530

    fourth commit

--- src/practicum/ArrayUtilities.java
+++ src/practicum/ArrayUtilities.java
@@ -4,5 +4,6 @@ public class ArrayUtilities {
     public static void main(String[] args) {
         System.out.println("one");
         System.out.println("Two");
+        System.out.println("Three");
     }
 }
